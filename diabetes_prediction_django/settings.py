3d80afe96abb6f104855d9c739b919c34309ad7c
Surya-the-coder <naansuryayadav@gmail.com>
2023-03-25 21:43:28 +0530

    init

--- diabetes_prediction_django/settings.py
+++ diabetes_prediction_django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^$8^lmh(+76x2^(g8_#x6qyysu7qzid59^ex)vbsliq$_y%-54
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
